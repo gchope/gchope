a116ef55876a60de0112375b605d234eb3b143a5
David Zhao <dzhao650@gmail.com>
2025-02-07 19:29:02 -0600

    Update init script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
 print auth sufficient pam_tid.so | sudo tee /etc/pam.d/sudo_local
 curl https://someonewhocares.org/hosts/hosts | sudo tee /etc/hosts
-curl https://raw.githubusercontent.com/torvalds/linux/refs/heads/master/.clang-format -o ~/.clang-format
+curl https://raw.githubusercontent.com/torvalds/linux/HEAD/.clang-format | tee ~/.clang-format
 git clone https://github.com/github/gitignore.git ~/gitignore
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScrolls -bool false
+curl https://raw.githubusercontent.com/elmcitydavid/elmcitydavid/HEAD/Dune.terminal | tee ~/Dune.terminal
