33c544670be97a88d92e64d9419aa06c740daf28
David Zhao <dzhao650@gmail.com>
2025-02-22 21:07:32 -0600

    username update

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/env zsh
+#! /usr/bin/env zsh
 print auth sufficient pam_tid.so | sudo tee /etc/pam.d/sudo_local
 curl https://someonewhocares.org/hosts/hosts | sudo tee /etc/hosts
 curl https://raw.githubusercontent.com/torvalds/linux/HEAD/.clang-format | tee ~/.clang-format
 git clone https://github.com/github/gitignore.git ~/gitignore
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScrolls -bool false
-curl https://raw.githubusercontent.com/elmcitydavid/elmcitydavid/HEAD/Dune.terminal | tee ~/Dune.terminal
+curl https://raw.githubusercontent.com/gchope/gchope/HEAD/Dune.terminal | tee ~/Dune.terminal
