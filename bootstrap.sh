03a3c1153b55387638a6d7d60cf9662c4817f7c7
David Zhao <dzhao650@gmail.com>
2025-02-23 16:19:02 -0600

    code style

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,19 @@
 #! /usr/bin/env zsh
+# will require sudo password
 print auth sufficient pam_tid.so | sudo tee /etc/pam.d/sudo_local
-curl https://someonewhocares.org/hosts/hosts | sudo tee /etc/hosts
-curl https://raw.githubusercontent.com/torvalds/linux/HEAD/.clang-format | tee ~/.clang-format
-git clone https://github.com/github/gitignore.git ~/gitignore
-defaults write -g ApplePressAndHoldEnabled -bool false
-defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScrolls -bool false
-curl https://raw.githubusercontent.com/gchope/gchope/HEAD/Dune.terminal | tee ~/Dune.terminal
+
+config() {
+	curl https://someonewhocares.org/hosts/hosts | sudo tee /etc/hosts
+	curl https://raw.githubusercontent.com/torvalds/linux/HEAD/.clang-format | tee ~/.clang-format
+	curl https://raw.githubusercontent.com/gchope/gchope/HEAD/Dune.terminal | tee ~/Dune.terminal
+}
+gitignore() {
+	git clone https://github.com/github/gitignore.git ~/gitignore
+}
+settings() {
+	defaults write -g ApplePressAndHoldEnabled -bool false
+	defaults write com.google.Chrome AppleEnableMouseSwipeNavigateWithScrolls -bool false
+}
+config
+gitignore
+settings
